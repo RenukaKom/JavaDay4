3043c3f59dd0c16b7ad5946da9c0865ac9899d30
chaudharineha2108@gmail.com <chaudharineha2108@gmail.com>
2022-06-22 23:38:21 +0530

    Added: Snake and Ladder only 1 player starts

---
+++ SnakeLadder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.snakeladder;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        System.out.println("Snake and Ladder Game Started");
+        int player = (int) Math.floor(Math.random() * (1) + 1);
+        System.out.println("Player " + player + " starts the game ");
+        int steps = 0;
+        System.out.println("Player started game from step " + steps);
+    }
+}
